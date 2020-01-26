6da58fcce55d49fbe531ca52177e18a59af3d233
ktpi2000 <katapi2000@gmail.com>
2020-01-27 02:26:46 +0900

    HTTP通信の返り値の型定義

--- src/app/article/article.page.ts
+++ src/app/article/article.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-article',
@@ -9,12 +11,39 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 export class ArticlePage implements OnInit {
   ID: number;
 
-  constructor(public route: ActivatedRoute) { }
+  //記事データを格納する配列
+  //初期値はnull
+  post: {
+    ID: number;
+    title: string;
+    content: string;
+    date: string;
+  } = {
+      ID: null,
+      title: null,
+      content: null,
+      date: null,
+    };
+
+  constructor(public route: ActivatedRoute, public http: HttpClient) { }
 
   ngOnInit() {
     this.route.paramMap
       .subscribe((params: ParamMap) => {
         this.ID = parseInt(params.get('articleId'), 10);
+      })
+  }
+
+  //ページ表示アニメーションが終了したら発火
+  ionViewDidEnter() {
+    this.http.get<{
+      ID: number;
+      title: string;
+      content: string;
+      date: string;
+    }>('https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts/' + this.ID)
+      .subscribe(data => {
+        this.post = data;
       })
   }
 
